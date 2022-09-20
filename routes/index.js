8ec4846c2b9311bf1f9a6359e6138b39ee6f836d
chahatjain2002 <77768829+chahatjain2002@users.noreply.github.com>
2022-09-20 22:59:21 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ connection.connect(function(err){
   }
 });
 /* GET home page. */
-router.get('/index', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
